57129f576dcad86135878ebd5505b96580ee11cb
Olek Osikowicz <tupolewcx@gmail.com>
2024-01-30 13:59:06 +0000

    Add determinism check

--- leaderboard/scripts/run_determism_check.sh
+++ leaderboard/scripts/run_determism_check.sh
@@ -14,7 +14,7 @@ export CHALLENGE_TRACK_CODENAME=SENSORS
 export PORT=2000 # same as the carla server port
 export TM_PORT=2500 # port for traffic manager, required when spawning multiple servers/clients
 export DEBUG_CHALLENGE=1
-export REPETITIONS=1 # multiple evaluation runs
+
 export ROUTES=leaderboard/data/training_routes/routes_town05_long.xml
 export TEAM_AGENT=leaderboard/team_code/interfuser_agent.py # agent
 export TEAM_CONFIG=leaderboard/team_code/interfuser_config.py # model checkpoint, not required for expert
@@ -23,19 +23,15 @@ export SCENARIOS=leaderboard/data/scenarios/town05_all_scenarios.json
 export SAVE_PATH=data/eval # path for saving episodes while evaluating
 export RESUME=True
 
+fps=20 #default
+substepping=1 #on default
+export REPETITIONS=5 # multiple evaluation runs
 
-fps_values=(20 15 10 5)
-substepping_values=(0 1) #false true
+# where to save results
+export CHECKPOINT_ENDPOINT="results/determinism_check_${REPETITIONS}_repetitions.json"
 
-for fps in "${fps_values[@]}"
-do
-    for substepping in "${substepping_values[@]}"
-    do
-        # where to save results
-        export CHECKPOINT_ENDPOINT="results/fidelity_eval_${fps}_fps_substepping_${substepping}.json"
-        echo $CHECKPOINT_ENDPOINT
 
-        python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
+python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
 --scenarios=${SCENARIOS}  \
 --routes=${ROUTES} \
 --repetitions=${REPETITIONS} \
@@ -50,6 +46,3 @@ do
 --trafficManagerPort=${TM_PORT} \
 --fps=${fps} \
 --substepping=${substepping}
-
-    done
-done
